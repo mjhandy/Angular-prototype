335230647331e432e3606a662961496ed96f68a7
Mark Handy <mjhandy@outlook.com>
2023-08-28 11:11:57 -0300

    base typography page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { 
-    path: '', 
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+    path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
   },
   { 
-    path: 'privacy-policy', 
-    loadChildren: () => import('./pages/privacy-policy/privacy-policy.module').then(m => m.PrivacyPolicyModule) 
+    path: 'privacy-policy', loadChildren: () => import('./pages/privacy-policy/privacy-policy.module').then(m => m.PrivacyPolicyModule) 
   },
-  { path: 'terms-conditions', loadChildren: () => import('./pages/terms/terms.module').then(m => m.TermsModule) },
+  { 
+    path: 'terms-conditions', loadChildren: () => import('./pages/terms/terms.module').then(m => m.TermsModule) 
+  },
+  { 
+    path: 'typography', loadChildren: () => import('./pages/typography/typography.module').then(m => m.TypographyModule) 
+  },
 ];
 
 @NgModule({
