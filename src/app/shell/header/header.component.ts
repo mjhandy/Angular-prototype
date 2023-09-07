22c7fba07ce0068f5908368f0fdff8e0379b9b37
Mark J. Handy <mjhandy@outlook.com>
2023-09-07 10:03:38 -0300

    Fix for routing issues

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports : [ MatIconModule, LoginComponent]
+  imports : [ MatIconModule, LoginComponent,RouterModule]
 })
 export class HeaderComponent {
 
