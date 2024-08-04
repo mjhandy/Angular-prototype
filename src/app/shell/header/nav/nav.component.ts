61fb4170553221c2058d0d1d886dc2bff3ae4d9f
Mark J. Handy <mjhandy@outlook.com>
2024-08-04 14:17:06 -0300

    * nav offcanvas view for mobile

--- src/app/shell/header/nav/nav.component.ts
+++ src/app/shell/header/nav/nav.component.ts
@@ -1,31 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LocalStorageService } from 'src/app/shared/services/local-storage-service';
-import { NgbNav, NgbDropdownModule  } from '@ng-bootstrap/ng-bootstrap';
-import {RouterModule} from '@angular/router';
+import { NgbNav, NgbDropdownModule, NgbOffcanvas, NgbOffcanvasConfig } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
-    TranslateModule, 
-    NgbNav, 
+    TranslateModule,
+    NgbNav,
     NgbDropdownModule,
-    RouterModule 
+    RouterModule
   ],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
+  providers: [NgbOffcanvasConfig, NgbOffcanvas]
 })
 export class NavComponent {
-  collapsed = true;
+  // collapsed = true;
 
   constructor(
+    config: NgbOffcanvasConfig,
+    private offcanvasService: NgbOffcanvas,
     private LocalStorageSerivce: LocalStorageService,
-  ){}
+  ) {
+    config.position = 'end';
+    config.backdropClass = 'bg-info';
+    config.keyboard = false;
 
-  ngOnInit(){
+  }
+
+  ngOnInit() {
     let lang = this.LocalStorageSerivce.getItem('lang');
 
-    console.debug('header lang get from local storage:', lang); 
+    console.debug('header lang get from local storage:', lang);
+  }
+
+  open(content: TemplateRef<any>) {
+    this.offcanvasService.open(content);
   }
 }
