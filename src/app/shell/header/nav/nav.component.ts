9dee0c08dba44f498794b9c106d071c777526f38
Mark J. Handy <mjhandy@outlook.com>
2024-08-05 17:34:33 -0300

    Revert "* nav offcanvas view for mobile"
    This reverts commit 61fb4170553221c2058d0d1d886dc2bff3ae4d9f.

--- src/app/shell/header/nav/nav.component.ts
+++ src/app/shell/header/nav/nav.component.ts
@@ -1,43 +1,31 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LocalStorageService } from 'src/app/shared/services/local-storage-service';
-import { NgbNav, NgbDropdownModule, NgbOffcanvas, NgbOffcanvasConfig } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
+import { NgbNav, NgbDropdownModule  } from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
-    TranslateModule,
-    NgbNav,
+    TranslateModule, 
+    NgbNav, 
     NgbDropdownModule,
-    RouterModule
+    RouterModule 
   ],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss',
-  providers: [NgbOffcanvasConfig, NgbOffcanvas]
+  styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-  // collapsed = true;
+  collapsed = true;
 
   constructor(
-    config: NgbOffcanvasConfig,
-    private offcanvasService: NgbOffcanvas,
     private LocalStorageSerivce: LocalStorageService,
-  ) {
-    config.position = 'end';
-    config.backdropClass = 'bg-info';
-    config.keyboard = false;
+  ){}
 
-  }
-
-  ngOnInit() {
+  ngOnInit(){
     let lang = this.LocalStorageSerivce.getItem('lang');
 
-    console.debug('header lang get from local storage:', lang);
-  }
-
-  open(content: TemplateRef<any>) {
-    this.offcanvasService.open(content);
+    console.debug('header lang get from local storage:', lang); 
   }
 }
