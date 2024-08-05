48c935da52afd0bcc33efa7e963041846103e04c
Mark J. Handy <mjhandy@outlook.com>
2024-08-05 19:56:42 -0300

    Header layout changes
    Updated the desktop view for the header

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -4,7 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { LangToggleComponent } from '../../shared/lang-toggle/lang-toggle.component';
+import { LangToggleComponent } from './lang-toggle/lang-toggle.component';
 import { LoginComponent } from './login/login.component';
 import { LocalStorageService } from 'src/app/shared/services/local-storage-service';
 
